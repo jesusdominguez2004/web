aa8e03643081850ab0031918692d33bb7b4617c7
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-01-22 20:51:58 -0500

    3d cube animation

---
+++ 3d-cube-animation/script.js
@@ -0,0 +1,40 @@
+// 3D Cube Animation: JavaScript
+// Author: https://youtu.be/-laon9XCNio?si=B9NWq90EfYt2kCwM
+document.addEventListener('DOMContentLoaded', function () {
+    let cube = document.querySelector('.cube');
+    let grids = document.querySelectorAll('.grid');
+
+    grids.forEach(grid => {
+        for (let i = 0; i < 100; i++) {
+            let span = document.createElement('span');
+            grid.appendChild(span);
+        }
+    });
+
+    function addRandomActiveClass() {
+        grids.forEach(grid => {
+            let spans = grid.querySelectorAll('span');
+            let randomIndex = Math.floor(Math.random() * spans.length);
+            spans[randomIndex].classList.add('active');
+
+            let removeTime = Math.floor(Math.random() + 1000) + 500;
+            setTimeout(() => {
+                spans[randomIndex].classList.remove('active');
+            }, removeTime);
+        });
+    }
+
+    function randomInterval() {
+        let interval = Math.floor(Math.random() * 200) + 100;
+        addRandomActiveClass();
+        setTimeout(randomInterval, interval);
+    }
+
+    randomInterval();
+
+    document.addEventListener('mousemove', (e) => {
+        let x = e.clientX / window.innerWidth - 0.5;
+        let y = e.clientY / window.innerHeight - 0.5;
+        cube.style.transform = `rotateX(${y * 360}deg) rotateY(${x * 360}deg)`;
+    });
+});
